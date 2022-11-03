d830893e9d6ee953ef8e6b754bb77c2c05fb340e
Daniel <danielukoha101@gmail.com>
2022-11-03 21:41:22 +0100

    style: updating footer horizontal line

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="container mx-auto px-4 py-8 flex flex-col gap-4 justify-between md:items-center md:flex-row">
+    <footer className="container mx-auto px-4 py-8 flex flex-col gap-4 justify-between  md:items-center md:flex-row">
+      <hr className="text-gray my-2" />
       <div className="w-32 md:w-36">
         <img src="/Zuri.Internship_Logo.png" className="w-full" />
       </div>
